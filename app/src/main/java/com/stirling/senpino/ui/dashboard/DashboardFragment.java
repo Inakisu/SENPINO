87f3b598202c2609b17c7f36c80784b0ebb6f58f
Iñaki <correode.iz@gmail.com>
2020-06-29 15:26:39 +0200

    Prueba gráfico

--- app/src/main/java/com/stirling/senpino/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/stirling/senpino/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,10 @@
 package com.stirling.senpino.ui.dashboard;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +15,79 @@ import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.stirling.senpino.Measurement;
 import com.stirling.senpino.R;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
 
-
+    private LineChart chart;
+    SharedPreferences preferences;
+    private ArrayList<Measurement> arListMeas;
+    private int lastX = 0;
+    private List<Entry> entries = new ArrayList<Entry>();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //get shared preferences
+        preferences = getActivity().getSharedPreferences("preferencias",
+                Context.MODE_PRIVATE);
+
+        arListMeas = new ArrayList<Measurement>();
+
+        //initialize chart
+        chart = (LineChart) view.findViewById(R.id.chart);
+        initializeChart(chart);
+
+
+
+
+    }
+
+    private void initializeChart(LineChart chart){
+        String jsonArList = preferences.getString("MARRAYKEY","");
+        Type type = new TypeToken<ArrayList<Measurement>>(){}.getType();
+        Gson gson = new Gson();
+        arListMeas = gson.fromJson(jsonArList, type);
+        if(!arListMeas.isEmpty()){
+            for (int i = 0; arListMeas.size() > i ; i++){
+                lastX++;
+//                Log.i("arList","Meas List from SP: " + arListMeas.get(i).getUser()
+//                        +", " + arListMeas.get(i).getTimestamp() + ", " + arListMeas.get(i).getWeight());
+                entries.add(new Entry(lastX,Float.parseFloat(arListMeas.get(i).getWeight())));
+            }
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+
+        dataSet.setColor(ContextCompat.getColor(getContext(), R.color.colorDataSet));
+        Description description = chart.getDescription();
+        Legend legend = chart.getLegend();
+//        dataSet.setValueTextColor(...); // styling, ...
+        LineData lineData = new LineData(dataSet);
+        legend.setEnabled(false);
+        description.setText(" ");
+        //lineData.set
+        chart.setData(lineData);
+        chart.notifyDataSetChanged(); // let the chart know it's data changed
+        chart.invalidate(); // refresh
+
+
     }
 }
